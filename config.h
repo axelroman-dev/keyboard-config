1bc48dbc0525ec2d9df4f01a003e903ef58dede3
ACEJUDO <axel.cejudo@kalte-logistik.com.mx>
2026-06-01 12:54:39 -0600

    Habilitar VIA y simplificar configuración RGB
    Habilita la configuración del teclado mediante la GUI de VIA, sustituyendo la funcionalidad de MOUSEKEY.
    
    Simplifica los ajustes de luz RGB eliminando la mayoría de los efectos y estableciendo un color blanco predeterminado.

--- config.h
+++ config.h
@@ -17,20 +17,10 @@
 
 /* ─── RGB ────────────────────────────────────────────────────────────────*/
 #ifdef RGBLIGHT_ENABLE
-    #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    #define RGBLIGHT_EFFECT_SNAKE
-    #define RGBLIGHT_EFFECT_KNIGHT
-    #define RGBLIGHT_EFFECT_CHRISTMAS
-    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    #define RGBLIGHT_EFFECT_RGB_TEST
-    #define RGBLIGHT_EFFECT_ALTERNATING
-    #define RGBLIGHT_EFFECT_TWINKLE
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
+    #define RGBLIGHT_DEFAULT_HUE 0    // blanco (sin matiz)
+    #define RGBLIGHT_DEFAULT_SAT 0    // saturación 0 = blanco puro
+    #define RGBLIGHT_LIMIT_VAL   120
+    #define RGBLIGHT_VAL_STEP    17
 #endif
 
 /* ─── Split ──────────────────────────────────────────────────────────────*/
--- rules.mk
+++ rules.mk
@@ -1,4 +1,4 @@
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
 OLED_ENABLE            = yes
-MOUSEKEY_ENABLE        = yes
+VIA_ENABLE             = yes
